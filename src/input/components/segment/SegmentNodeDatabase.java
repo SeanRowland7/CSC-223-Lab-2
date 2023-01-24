2f7755c85a3afd81f4d7855b1a7a3d37af31f50f
juliaehogg <122399945+juliaehogg@users.noreply.github.com>
2023-01-24 16:30:45 -0500

    seg node database

--- src/input/components/segment/SegmentNodeDatabase.java
+++ src/input/components/segment/SegmentNodeDatabase.java
@@ -1,5 +1,31 @@
 package input.components.segment;
 
-public class SegmentNodeDatabase {
+import input.components.point.*;
 
+import java.util.Map;
+import java.util.Set;
+
+public class SegmentNodeDatabase
+{
+
+	protected Map<PointNode, Set<PointNode>> _adjLists;
+	
+	public SegmentNodeDatabase()
+	{
+		// todo
+	}
+	
+	public SegmentNodeDatabase(Map<PointNode, Set<PointNode>>)
+	{
+		// todo
+	}
+	
+	public int numUndirectedEdges()
+	{
+		
+	}
+	
+	
+	
+	
 }
