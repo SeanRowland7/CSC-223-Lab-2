9ed019976169183ab1b62065cd15d27b7c377879
SeanRowland7 <seanwrowland@gmail.com>
2023-01-25 13:05:56 -0500

    Worked On SegmentNodeDatabase

--- src/input/components/segment/SegmentNodeDatabase.java
+++ src/input/components/segment/SegmentNodeDatabase.java
@@ -2,7 +2,10 @@ package input.components.segment;
 
 import input.components.point.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -24,12 +27,28 @@ public class SegmentNodeDatabase
 	
 	public int numUndirectedEdges()
 	{
-
+		//Collection<Set<PointNode>> adjValuesList = _adjLists.values();
+		
+		int count = 0;
+		
+		for( Set<PointNode> adjValueList : _adjLists.values())
+		{
+			count += adjValueList.size();
+		}
+		
+		return count;
 	}
 	
 	private void addDirectedEdge(PointNode p1, PointNode p2)
 	{
-		//_adjLists.put(p1, ? )
+		// Create a copy of the adjacency list for p1.
+		LinkedHashSet<PointNode> adjList = new LinkedHashSet<PointNode>(_adjLists.get(p1));
+		 
+		// Add the new edge to that adjacency list 
+		adjList.add(p2);
+		
+		// Replace the old adjacency list with the new one.
+		_adjLists.put(p1, adjList);
 	}
 		
 	public void addUndirectedEdge(PointNode p1, PointNode p2)
@@ -40,17 +59,31 @@ public class SegmentNodeDatabase
 	
 	public void addAdjacencyList(PointNode p, List<PointNode> list)
 	{
-		
+		if( list instanceof Set) _adjLists.put(p, (Set<PointNode>)list);
 	}
 	
 	public List<SegmentNode> asSegmentList()
 	{
+		// Try using entrySet()
+	
+		List<SegmentNode> listSegNodes = new ArrayList<SegmentNode>();
 		
+		for( Set<PointNode> adjValueList : _adjLists.values())
+		{
+			for(PointNode p : adjValueList)
+			{
+				//listSegNodes.add(new SegmentNode(, p)):
+			}
+			
+		}
+		
+		return listSegNodes;
 	}
 	
 	public List<SegmentNode> asUniqueSegmentList()
 	{
-		
+		List<SegmentNode> listSegNodes = new ArrayList<SegmentNode>();
+		return listSegNodes;
 	}
 	
 }
