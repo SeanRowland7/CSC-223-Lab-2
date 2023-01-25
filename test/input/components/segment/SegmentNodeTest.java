843da26d359c31a6180c8921fb322842cf7cea6a
SeanRowland7 <seanwrowland@gmail.com>
2023-01-25 00:38:37 -0500

    Tested SegmentNode

--- test/input/components/segment/SegmentNodeTest.java
+++ test/input/components/segment/SegmentNodeTest.java
@@ -11,44 +11,54 @@ class SegmentNodeTest
 	@Test
 	void testSegmentNode() 
 	{
-		assertEquals("A(-3.4, 1.9)", new PointNode("A", -3.4, 1.9).toString());
+		assertEquals("A(1.0, 2.0) B(3.0, 4.0)\n", new SegmentNode(new PointNode("A", 1.0, 2.0), new PointNode("B", 3.0, 4.0)).toString());
 	}
 	
 	@Test
 	void testEqualsBadInput()
 	{
-		assertFalse(new PointNode(1.0, 1.0).equals(null));
-		assertFalse(new PointNode(1.0, 1.0).equals(":("));
+		assertFalse(new SegmentNode(new PointNode("A", 1.0, 2.0), new PointNode("B", 3.0, 4.0)).equals(null));
 	}
 
 	@Test
 	void testEqualsIntegers()
 	{
-		assertTrue(new PointNode(1, 1).equals(new PointNode(1, 1)));
-		assertTrue(new PointNode(3, -5).equals(new PointNode(3, -5)));
-		assertFalse(new PointNode(-5, 3).equals(new PointNode(3, -5)));
-		assertFalse(new PointNode(-5, -5).equals(new PointNode(3, 3)));
+		SegmentNode n1 = new SegmentNode(new PointNode("A", 1.0, 2.0), new PointNode("B", 3.0, 4.0));
+		SegmentNode n2 = new SegmentNode(new PointNode("A", 1.0, 2.0), new PointNode("B", 3.0, 4.0));
+		SegmentNode n3 = new SegmentNode(new PointNode("C", 3.0, 4.0), new PointNode("D", 1.0, 2.0));
+		SegmentNode n4 = new SegmentNode(new PointNode("E", 1.0, 2.0), new PointNode("F", 4.0, 3.0));
+		
+		assertTrue(n1.equals(n2));
+		assertTrue(n1.equals(n3));
+		assertTrue(n3.equals(n2));
+		assertFalse(n1.equals(n4));
+		assertFalse(n3.equals(n4));
 	}
 	
 	@Test
 	void testEqualsDoublesBarelyOff()
 	{
-		assertTrue(new PointNode(1.0000000001, 1).equals(new PointNode(1, 1)));
-		assertTrue(new PointNode(3.1415926, -12).equals(new PointNode(3.14159265, -12.0)));
+		SegmentNode n1 = new SegmentNode(new PointNode("A", 1.0000001, 3.14159265), new PointNode("B", 1.41421356, 4));
+		SegmentNode n2 = new SegmentNode(new PointNode("A", 1, 3.141592), new PointNode("B", 1.41421399, 4.00000007));
+		
+		assertTrue(n1.equals(n2));
+		assertTrue(n2.equals(n1));
 	}
 	
 	@Test
 	void testEqualsDoublesOffBySignificant()
 	{
-		assertFalse(new PointNode(1.001, 1).equals(new PointNode(1, 1)));
-		assertFalse(new PointNode(3.1415926, -12).equals(new PointNode(3.14, -12.0)));
+		SegmentNode n1 = new SegmentNode(new PointNode("A", 1.0030001, 3.14159265), new PointNode("B", 1.41421356, 4));
+		SegmentNode n2 = new SegmentNode(new PointNode("A", 1, 3.151592), new PointNode("B", 1.41621399, 4.04000007));
+		
+		assertFalse(n1.equals(n2));
+		assertFalse(n2.equals(n1));
 	}
 	
 	@Test
 	void testToString() 
 	{
-		assertEquals("__UNNAMED(1.0, 1.0)", new PointNode(1.0, 1.0).toString());
-		assertEquals("A(-3.4, 1.9)", new PointNode("A", -3.4, 1.9).toString());
+		assertEquals("A(1.0, 2.0) B(3.0, 4.0)\n", new SegmentNode(new PointNode("A", 1.0, 2.0), new PointNode("B", 3.0, 4.0)).toString());
 	}
 }
 
